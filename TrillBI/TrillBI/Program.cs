dd6f999e5227acb27b7c0231d878fdc167515b16
tntokum <tntokum@gmail.com>
2020-09-23 13:45:23 -0600

    Initial Trill networking solution

---
+++ TrillBI/TrillBI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Trill + networking imports
+using RxSockets;
+using System.Reactive.Linq;
+using Microsoft.StreamProcessing;
+
+namespace TrillBI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
