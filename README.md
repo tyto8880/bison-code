cc29f9e2ce3a8097af180be38775ebf39be1acbc
Tyler Tokumoto <tntokum@gmail.com>
2020-09-24 11:14:00 -0600

    Successful Bytes to Observable conversion Updated README

---
+++ README.md
@@ -0,0 +1,9 @@
+# BISON
+
+## tyto8880's branch
+
+-------
+
+Currently working on:
+
+socket => trill streamable
--- TrillBI/TrillBI/Program.cs
+++ TrillBI/TrillBI/Program.cs
@@ -4,70 +4,104 @@ using System.Net.Sockets;
 using System.Text;
 
 // Trill + networking imports
+using RxSockets;
 using System.Reactive.Linq;
 using Microsoft.StreamProcessing;
+using System.Threading.Tasks;
 
 namespace TrillBI {
     class Program {
-        
-        // Incoming data from the client.
-        public static string data = null;
-
         private struct LocationData {
-            public LocationData(long latitude, long longitude) {
+            public LocationData(double latitude, double longitude) {
                 this.latitude = latitude;
                 this.longitude = longitude;
             }
 
-            public long latitude;
-            public long longitude;
+            public double latitude;
+            public double longitude;
+
+            public override string ToString() => "Location Data: " + latitude + ", " + longitude;
         }
-        static void Main(string[] args) {
+
+        // Incoming data from the client.
+        private static LocationData[] data = new LocationData[10];
+
+        static async Task Main(string[] args) {
             // start networking
             String ip = "127.0.0.1";
             int port = 8000;
+
+            // maybe move to new method
+
+            // make incoming data observable
+            IObservable<LocationData> inputObservable = data.ToObservable();
+            IStreamable<Empty, LocationData> inputStream;
+            
             StartListener(ip, port);
 
-            
+            await inputObservable.ForEachAsync(m => Console.WriteLine(m));
         }
 
         static void StartListener(String ip, int port) {
             byte[] bytes = new byte[1024];
             IPAddress ipAddress = IPAddress.Parse(ip);
             IPEndPoint localEndpoint = new IPEndPoint(ipAddress, port);
+
+            // make non-blocking at some point
             Socket server = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             Console.WriteLine(localEndpoint.ToString());
 
+            //IRxSocketServer server = RxSocketServer.CreateOnEndPoint(localEndpoint);
+            //server.AcceptObservable.Subscribe(onNext: acceptClient =>
+            //{
+            //    // prints out characters received
+            //    // want to put into byte array eventually though, so...
+            //    //acceptClient.ReceiveObservable.ForEachAsync(m => Console.Write(Convert.ToChar(m)));
+
+            //    // let class var data hold the Observable containing bytes
+            //    data = acceptClient.ReceiveObservable;
+            //});
+
+            //await Task.Delay(3000);
+
+            //// Disconnect.
+            //await server.DisposeAsync();
+
             try {
                 server.Bind(localEndpoint);
                 server.Listen(10);
-                
+
                 while (true) {
                     Console.WriteLine("Waiting for a connection...");
                     // Program is suspended while waiting for an incoming connection.  
                     Socket handler = server.Accept();
-                    data = null;
+                    string socketdata = null;
 
                     while (true) {
                         int bytesRec = handler.Receive(bytes);
-                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
                         if (bytesRec > 0) {
-                            Console.WriteLine("Bytes rec'd: {0}\tData so far : {1}", bytesRec, data);
+                            socketdata += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                            Console.WriteLine("Bytes rec'd: {0}\tData so far : {1}", bytesRec, socketdata);
                         } else {
-                            break;
-                        }
-                        if (data.IndexOf("<EOF>") > -1) {
                             break;
                         }
                     }
 
                     // Show the data on the console.  
-                    Console.WriteLine("Text received : {0}", data);
+                    Console.WriteLine("Text received : {0}", socketdata);
+                    data[0] = ParseInput(socketdata);
+                    break;
                 }
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
             }
+        }
+
+        private static LocationData ParseInput(string input) {
+            string[] values = input.Split(new string[] { ", " }, StringSplitOptions.None);
+            Console.WriteLine(values[0] + ", " + values[1]);
+            return new LocationData(Convert.ToDouble(values[0]), Convert.ToDouble(values[1]));
         }
     }
 }
