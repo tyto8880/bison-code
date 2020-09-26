416eeef3d1729a0126fda96be5fccd63d813bbbf
Tyler Tokumoto <tntokum@gmail.com>
2020-09-25 19:18:34 -0600

    Ingressing data into Streamable

--- TrillBI/TrillBI/Program.cs
+++ TrillBI/TrillBI/Program.cs
@@ -12,19 +12,21 @@ using System.Threading.Tasks;
 namespace TrillBI {
     class Program {
         private struct LocationData {
-            public LocationData(double latitude, double longitude) {
+            public LocationData(double latitude, double longitude, long time) {
                 this.latitude = latitude;
                 this.longitude = longitude;
+                this.time = time;
             }
 
             public double latitude;
             public double longitude;
+            public long time;
 
             public override string ToString() => "Location Data: " + latitude + ", " + longitude;
         }
 
         // Incoming data from the client.
-        private static LocationData[] data = new LocationData[10];
+        private static LocationData[] data = new LocationData[3];
 
         static async Task Main(string[] args) {
             // start networking
@@ -36,19 +38,24 @@ namespace TrillBI {
             // make incoming data observable
             IObservable<LocationData> inputObservable = data.ToObservable();
             IStreamable<Empty, LocationData> inputStream;
-            
-            StartListener(ip, port);
 
-            await inputObservable.ForEachAsync(m => Console.WriteLine(m));
+            StartListener(ip, port);
+            inputStream = inputObservable.Select(r => StreamEvent.CreateInterval(r.time, r.time + 1, r)).ToStreamable();
+            inputStream.ToStreamEventObservable().ForEachAsync(m => Console.WriteLine(m)).Wait();
+
+            Console.WriteLine("Done. Press ENTER to terminate");
+            Console.ReadLine();
         }
 
         static void StartListener(String ip, int port) {
             byte[] bytes = new byte[1024];
             IPAddress ipAddress = IPAddress.Parse(ip);
             IPEndPoint localEndpoint = new IPEndPoint(ipAddress, port);
+            int index = 0;
 
             // make non-blocking at some point
             Socket server = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //server.Blocking = false;
 
             Console.WriteLine(localEndpoint.ToString());
 
@@ -76,6 +83,7 @@ namespace TrillBI {
                     Console.WriteLine("Waiting for a connection...");
                     // Program is suspended while waiting for an incoming connection.  
                     Socket handler = server.Accept();
+                    handler.Blocking = false;
                     string socketdata = null;
 
                     while (true) {
@@ -90,18 +98,21 @@ namespace TrillBI {
 
                     // Show the data on the console.  
                     Console.WriteLine("Text received : {0}", socketdata);
-                    data[0] = ParseInput(socketdata);
-                    break;
+                    data[index] = ParseInput(socketdata, index);
+                    index += 1;
+                    if (index == 3) {
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
             }
         }
 
-        private static LocationData ParseInput(string input) {
+        private static LocationData ParseInput(string input, long time) {
             string[] values = input.Split(new string[] { ", " }, StringSplitOptions.None);
-            Console.WriteLine(values[0] + ", " + values[1]);
-            return new LocationData(Convert.ToDouble(values[0]), Convert.ToDouble(values[1]));
+            //Console.WriteLine(values[0] + ", " + values[1]);
+            return new LocationData(Convert.ToDouble(values[0]), Convert.ToDouble(values[1]), time);
         }
     }
 }
--- client.py
+++ client.py
@@ -5,3 +5,19 @@ s = socket.socket()
 s.connect(('127.0.0.1', 8000))
 
 s.send(b'40.12071, -140.3291')
+
+s.close()
+
+s = socket.socket()
+s.connect(('127.0.0.1', 8000))
+
+s.send(b'400.12071, -140.3291')
+
+s.close()
+
+s = socket.socket()
+s.connect(('127.0.0.1', 8000))
+
+s.send(b'4000.12071, -140.3291')
+
+s.close()
